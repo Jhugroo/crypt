d868b43f811578baa57c81b69f0c2f6f016cb95b
Jhugroo <prakash.jhugroo@gmail.com>
2025-02-27 12:09:16 +0400

    #main

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,24 @@
-// place files you want to import through the `$lib` alias in this folder.
+import cjs from 'crypto-js';
+class StringEncryptor {
+	private passphrase: string | undefined;
+
+	constructor() {}
+	setPassPhrase(passphrase: string) {
+		this.passphrase = passphrase;
+	}
+	encrypt(text: string): string {
+		if (!this.passphrase) {
+			throw new Error('Passphrase not set');
+		}
+		return cjs.AES.encrypt(text, this.passphrase).toString();
+	}
+
+	decrypt(encryptedText: string): string {
+		if (!this.passphrase) {
+			throw new Error('Passphrase not set');
+		}
+		return cjs.AES.decrypt(encryptedText, this.passphrase).toString(cjs.enc.Utf8);
+	}
+}
+
+export default StringEncryptor;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,78 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import StringEncryptor from '$lib';
+	import Radio from '$lib/components/Radio.svelte';
+	let passField = $state('');
+	let textField = $state('');
+	let output = $state('');
+	let toEncrypt = $state(true);
+	const encryptor = new StringEncryptor();
+	function process() {
+		encryptor.setPassPhrase(passField);
+		output = toEncrypt ? encryptor.encrypt(textField) : encryptor.decrypt(textField);
+	}
+	const options = [
+		{
+			value: true,
+			label: 'encrypt'
+		},
+		{
+			value: false,
+			label: 'decrypt'
+		}
+	];
+	function highlightText(event: MouseEvent) {
+		const range = document.createRange();
+		event.target && range.selectNodeContents(event.target as Node);
+		const selection = window.getSelection();
+		selection?.removeAllRanges();
+		selection?.addRange(range);
+	}
+</script>
+
+<div>
+	<Radio {options} fontSize={16} legend="Select " bind:userSelected={toEncrypt} />
+	<div class="increase-padding">
+		<label>
+			Passphrase:
+			<input name="pass" type="pass" bind:value={passField} />
+		</label>
+	</div>
+	<div class="increase-padding">
+		<!-- svelte-ignore a11y_label_has_associated_control -->
+		<label> text </label>
+		<!-- svelte-ignore element_invalid_self_closing_tag -->
+		<textarea rows="4" name="text" bind:value={textField} />
+	</div>
+	<button class="increase-padding" onclick={process}
+		>{options.find((option) => option.value === toEncrypt)?.label ?? 'none selected'}
+	</button>
+	{#if output.length > 0}
+		<!-- svelte-ignore element_invalid_self_closing_tag -->
+		<div class="increase-padding">
+			<!-- svelte-ignore a11y_label_has_associated_control -->
+			<label class="width-max">{toEncrypt ? 'Encrypted text' : 'Decrypted text'}</label>
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<!-- svelte-ignore a11y_no_static_element_interactions -->
+			<textarea class=" width-max" onclick={highlightText} value={output} />
+		</div>
+	{/if}
+</div>
+
+<style>
+	textarea {
+		width: 100%;
+	}
+	.width-max {
+		width: 100%;
+	}
+	.increase-padding {
+		margin-top: 20px;
+		display: flex;
+		flex-direction: column;
+	}
+	span {
+		border: 2px solid black;
+		padding: 5px;
+		cursor: pointer;
+	}
+</style>
