7e008295b6aa7187f865980d28d41cd7c8b2b816
Jhugroo <prakash.jhugroo@gmail.com>
2025-02-27 20:25:50 +0400

    #main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import StringEncryptor from '$lib';
 	import Radio from '$lib/components/Radio.svelte';
+	import '@picocss/pico';
 	let passField = $state('');
 	let textField = $state('');
 	let output = $state('');
@@ -20,59 +21,50 @@
 			label: 'decrypt'
 		}
 	];
-	function highlightText(event: MouseEvent) {
-		const range = document.createRange();
-		event.target && range.selectNodeContents(event.target as Node);
-		const selection = window.getSelection();
-		selection?.removeAllRanges();
-		selection?.addRange(range);
-	}
 </script>
 
 <div>
 	<Radio {options} fontSize={16} legend="Select " bind:userSelected={toEncrypt} />
-	<div class="increase-padding">
+	<div>
 		<label>
 			Passphrase:
 			<input name="pass" type="pass" bind:value={passField} />
 		</label>
 	</div>
-	<div class="increase-padding">
+	<div>
 		<!-- svelte-ignore a11y_label_has_associated_control -->
 		<label> text </label>
 		<!-- svelte-ignore element_invalid_self_closing_tag -->
 		<textarea rows="4" name="text" bind:value={textField} />
 	</div>
-	<button class="increase-padding" onclick={process}
+	<button onclick={process}
 		>{options.find((option) => option.value === toEncrypt)?.label ?? 'none selected'}
 	</button>
 	{#if output.length > 0}
 		<!-- svelte-ignore element_invalid_self_closing_tag -->
-		<div class="increase-padding">
+		<div>
 			<!-- svelte-ignore a11y_label_has_associated_control -->
-			<label class="width-max">{toEncrypt ? 'Encrypted text' : 'Decrypted text'}</label>
+			<label>{toEncrypt ? 'Encrypted text' : 'Decrypted text'}</label>
 			<!-- svelte-ignore a11y_click_events_have_key_events -->
 			<!-- svelte-ignore a11y_no_static_element_interactions -->
-			<textarea class=" width-max" onclick={highlightText} value={output} />
+			<textarea rows="34" value={output} />
 		</div>
 	{/if}
 </div>
 
 <style>
-	textarea {
-		width: 100%;
+	:root {
+		--pico-border-radius: 2rem;
+		--pico-typography-spacing-vertical: 1.5rem;
+		--pico-form-element-spacing-vertical: 1rem;
+		--pico-form-element-spacing-horizontal: 1.25rem;
 	}
-	.width-max {
-		width: 100%;
+	h1 {
+		--pico-font-family: Pacifico, cursive;
+		--pico-font-weight: 400;
+		--pico-typography-spacing-vertical: 0.5rem;
 	}
-	.increase-padding {
-		margin-top: 20px;
-		display: flex;
-		flex-direction: column;
-	}
-	span {
-		border: 2px solid black;
-		padding: 5px;
-		cursor: pointer;
+	button {
+		--pico-font-weight: 700;
 	}
 </style>
